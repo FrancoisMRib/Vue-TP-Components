aab7f2d2dda7042a41723499972c572468f56ddc
FracoisMRib <fr.ribere@gmail.com>
2023-12-14 15:14:58 +0100

    Exo Composants séparation

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <le-header></le-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import LeHeader from './components/LeHeader.vue';
+export default {
+  components : {
+    'le-header' : LeHeader , 
+  } ,
+  data() {
+    return {
+      activeUser: {
+        name: 'LOL',
+        description: 'admin du site',
+        role: 'premium',
+      },
+    };
+  },
+};
+</script>
